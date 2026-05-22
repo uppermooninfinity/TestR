4cdd29c096d275d1946c5885e766c0b92f027cd3
uppermooninfinity <uppermooninfinity@gmail.com>
2026-05-22 14:04:17 +0530

    Update stream.py

--- Oneforall/utils/stream/stream.py
+++ Oneforall/utils/stream/stream.py
@@ -5,7 +5,7 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
-from Oneforall import YouTube, app
+from Oneforall import YouTube, app, Carbon
 from Oneforall.core.call import Hotty
 
 # Define a dictionary to track the last message timestamp for each user
@@ -17,7 +17,7 @@ SPAM_WINDOW_SECONDS = 5
 
 
 from pyrogram.types import InlineKeyboardMarkup
-from youtubesearchpython.__future__ import VideosSearch
+from py_yt import VideosSearch
 
 import config
 from Oneforall import Carbon, YouTube, app
@@ -32,6 +32,10 @@ from Oneforall.utils.inline import (
     stream_markup2,
 )
 from Oneforall.utils.stream.queue import put_queue, put_queue_index
+# PREMIUM THUMBNAIL IMPORT
+from Oneforall.utils.thumbnails import get_thumb
+from Oneforall.utils.stream.thumbnail import get_thumbnail_status
+
 
 async def stream(
     _,
@@ -49,7 +53,7 @@ async def stream(
     if not result:
         return
         # Define intro URL
-    intro_url = "https://files.catbox.moe/swa9ev.mp3"  #made by @sukuna_Dev
+    intro_url = "https://graph.org/file/29d71d09801ca0ae55cd8-4832d851713ae41c8f.mp4"  #made by @sukuna_Dev
 
     if forceplay:
         await Hotty.force_stop_stream(chat_id)
@@ -120,18 +124,33 @@ async def stream(
                 )
                 img = await get_thumb(vidid)
                 button = stream_markup(_, vidid, chat_id)
-                run = await app.send_photo(
-                    original_chat_id,
-                    photo=img,
-                    has_spoiler=True,
-                    caption=_["stream_1"].format(
-                        f"https://t.me/{app.username}?start=info_{vidid}",
-                        title[:18],
-                        duration_min,
-                        user_name,
-                    ),
-                    reply_markup=InlineKeyboardMarkup(button),
-                )
+                
+                # Check thumbnail status
+                thumb_status = get_thumbnail_status(chat_id)
+                if thumb_status == "on":
+                    run = await app.send_photo(
+                        original_chat_id,
+                        photo=img,
+                        has_spoiler=True,
+                        caption=_["stream_1"].format(
+                            f"https://t.me/{app.username}?start=info_{vidid}",
+                            title[:18],
+                            duration_min,
+                            user_name,
+                        ),
+                        reply_markup=InlineKeyboardMarkup(button),
+                    )
+                else:
+                    run = await app.send_message(
+                        original_chat_id,
+                        text=_["stream_1"].format(
+                            f"https://t.me/{app.username}?start=info_{vidid}",
+                            title[:18],
+                            duration_min,
+                            user_name,
+                        ),
+                        reply_markup=InlineKeyboardMarkup(button),
+                    )
 
                 db[chat_id][0]["mystic"] = run
                 db[chat_id][0]["markup"] = "stream"
@@ -181,15 +200,27 @@ async def stream(
             img = await get_thumb(vidid)
             position = len(db.get(chat_id)) - 1
             button = aq_markup(_, chat_id)
-            await app.send_photo(
-                chat_id=original_chat_id,
-                photo=img,
-                has_spoiler=True,
-                caption=_["queue_4"].format(
-                    position, title[:18], duration_min, user_name
-                ),
-                reply_markup=InlineKeyboardMarkup(button),
-            )
+            
+            # Check thumbnail status
+            thumb_status = get_thumbnail_status(chat_id)
+            if thumb_status == "on":
+                await app.send_photo(
+                    chat_id=original_chat_id,
+                    photo=img,
+                    has_spoiler=True,
+                    caption=_["queue_4"].format(
+                        position, title[:18], duration_min, user_name
+                    ),
+                    reply_markup=InlineKeyboardMarkup(button),
+                )
+            else:
+                await app.send_message(
+                    chat_id=original_chat_id,
+                    text=_["queue_4"].format(
+                        position, title[:18], duration_min, user_name
+                    ),
+                    reply_markup=InlineKeyboardMarkup(button),
+                )
         else:
             if not forceplay:
                 db[chat_id] = []
@@ -214,18 +245,33 @@ async def stream(
             )
             img = await get_thumb(vidid)
             button = stream_markup(_, vidid, chat_id)
-            run = await app.send_photo(
-                original_chat_id,
-                photo=img,
-                has_spoiler=True,
-                caption=_["stream_1"].format(
-                    f"https://t.me/{app.username}?start=info_{vidid}",
-                    title[:18],
-                    duration_min,
-                    user_name,
-                ),
-                reply_markup=InlineKeyboardMarkup(button),
-            )
+            
+            # Check thumbnail status
+            thumb_status = get_thumbnail_status(chat_id)
+            if thumb_status == "on":
+                run = await app.send_photo(
+                    original_chat_id,
+                    photo=img,
+                    has_spoiler=True,
+                    caption=_["stream_1"].format(
+                        f"https://t.me/{app.username}?start=info_{vidid}",
+                        title[:18],
+                        duration_min,
+                        user_name,
+                    ),
+                    reply_markup=InlineKeyboardMarkup(button),
+                )
+            else:
+                run = await app.send_message(
+                    original_chat_id,
+                    text=_["stream_1"].format(
+                        f"https://t.me/{app.username}?start=info_{vidid}",
+                        title[:18],
+                        duration_min,
+                        user_name,
+                    ),
+                    reply_markup=InlineKeyboardMarkup(button),
+                )
 
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "stream"
@@ -269,14 +315,27 @@ async def stream(
                 forceplay=forceplay,
             )
             button = stream_markup2(_, chat_id)
-            run = await app.send_photo(
-                original_chat_id,
-                photo=config.SOUNCLOUD_IMG_URL,
-                caption=_["stream_1"].format(
-                    config.SUPPORT_CHAT, title[:23], duration_min, user_name
-                ),
-                reply_markup=InlineKeyboardMarkup(button),
-            )
+            
+            # Check thumbnail status
+            thumb_status = get_thumbnail_status(chat_id)
+            if thumb_status == "on":
+                run = await app.send_photo(
+                    original_chat_id,
+                    photo=config.SOUNCLOUD_IMG_URL,
+                    caption=_["stream_1"].format(
+                        config.SUPPORT_CHAT, title[:23], duration_min, user_name
+                    ),
+                    reply_markup=InlineKeyboardMarkup(button),
+                )
+            else:
+                run = await app.send_message(
+                    original_chat_id,
+                    text=_["stream_1"].format(
+                        config.SUPPORT_CHAT, title[:23], duration_min, user_name
+                    ),
+                    reply_markup=InlineKeyboardMarkup(button),
+                )
+            
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "tg"
     elif streamtype == "telegram":
@@ -323,12 +382,23 @@ async def stream(
             if video:
                 await add_active_video_chat(chat_id)
             button = stream_markup2(_, chat_id)
-            run = await app.send_photo(
-                original_chat_id,
-                photo=config.TELEGRAM_VIDEO_URL if video else config.TELEGRAM_AUDIO_URL,
-                caption=_["stream_1"].format(link, title[:23], duration_min, user_name),
-                reply_markup=InlineKeyboardMarkup(button),
-            )
+            
+            # Check thumbnail status
+            thumb_status = get_thumbnail_status(chat_id)
+            if thumb_status == "on":
+                run = await app.send_photo(
+                    original_chat_id,
+                    photo=config.TELEGRAM_VIDEO_URL if video else config.TELEGRAM_AUDIO_URL,
+                    caption=_["stream_1"].format(link, title[:23], duration_min, user_name),
+                    reply_markup=InlineKeyboardMarkup(button),
+                )
+            else:
+                run = await app.send_message(
+                    original_chat_id,
+                    text=_["stream_1"].format(link, title[:23], duration_min, user_name),
+                    reply_markup=InlineKeyboardMarkup(button),
+                )
+            
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "tg"
     elif streamtype == "live":
@@ -336,7 +406,6 @@ async def stream(
         vidid = result["vidid"]
         title = (result["title"]).title()
         thumbnail = result["thumb"]
-        has_spoiler=True,
         duration_min = "Live Track"
         status = True if video else None
         if await is_active_chat(chat_id):
@@ -385,18 +454,34 @@ async def stream(
             )
             img = await get_thumb(vidid)
             button = stream_markup2(_, chat_id)
-            run = await app.send_photo(
-                original_chat_id,
-                photo=img,
-                has_spoiler=True,
-                caption=_["stream_1"].format(
-                    f"https://t.me/{app.username}?start=info_{vidid}",
-                    title[:23],
-                    duration_min,
-                    user_name,
-                ),
-                reply_markup=InlineKeyboardMarkup(button),
-            )
+            
+            # Check thumbnail status
+            thumb_status = get_thumbnail_status(chat_id)
+            if thumb_status == "on":
+                run = await app.send_photo(
+                    original_chat_id,
+                    photo=img,
+                    has_spoiler=True,
+                    caption=_["stream_1"].format(
+                        f"https://t.me/{app.username}?start=info_{vidid}",
+                        title[:23],
+                        duration_min,
+                        user_name,
+                    ),
+                    reply_markup=InlineKeyboardMarkup(button),
+                )
+            else:
+                run = await app.send_message(
+                    original_chat_id,
+                    text=_["stream_1"].format(
+                        f"https://t.me/{app.username}?start=info_{vidid}",
+                        title[:23],
+                        duration_min,
+                        user_name,
+                    ),
+                    reply_markup=InlineKeyboardMarkup(button),
+                )
+            
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "tg"
     elif streamtype == "index":
@@ -441,37 +526,23 @@ async def stream(
                 forceplay=forceplay,
             )
             button = stream_markup2(_, chat_id)
-            run = await app.send_photo(
-                original_chat_id,
-                photo=config.STREAM_IMG_URL,
-                caption=_["stream_2"].format(user_name),
-                reply_markup=InlineKeyboardMarkup(button),
-            )
+            
+            # Check thumbnail status
+            thumb_status = get_thumbnail_status(chat_id)
+            if thumb_status == "on":
+                run = await app.send_photo(
+                    original_chat_id,
+                    photo=config.STREAM_IMG_URL,
+                    caption=_["stream_2"].format(user_name),
+                    reply_markup=InlineKeyboardMarkup(button),
+                )
+            else:
+                run = await app.send_message(
+                    original_chat_id,
+                    text=_["stream_2"].format(user_name),
+                    reply_markup=InlineKeyboardMarkup(button),
+                )
+            
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "tg"
             await mystic.delete()
-
-
-# Function to get thumbnail by video ID
-async def get_thumb(videoid):
-    try:
-        # Search for the video using video ID
-        query = f"https://www.youtube.com/watch?v={videoid}"
-        results = VideosSearch(query, limit=1)
-        for result in (await results.next())["result"]:
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-        return thumbnail
-    except Exception as e:
-        return config.YOUTUBE_IMG_URL
-
-
-async def get_thumb(vidid):
-    try:
-        # Search for the video using video ID
-        query = f"https://www.youtube.com/watch?v={vidid}"
-        results = VideosSearch(query, limit=1)
-        for result in (await results.next())["result"]:
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-        return thumbnail
-    except Exception as e:
-        return config.YOUTUBE_IMG_URL
